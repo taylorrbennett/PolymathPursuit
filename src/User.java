6397980d22ee0102cbcf1376a26aef7e9f7ebdd9
taylorrbennett <taylorrbennett1998@gmail.com>
2026-06-01 20:12:46 +0200

    Just did User class tonight, need to work on TriviaQuestions and Scoring, By next Tuesday I want to have a very basic game running similiar to Java Project. Geeting help from Codedex and Sololearn in using Constructors, also using old Java project.

---
+++ src/User.java
@@ -0,0 +1,22 @@
+public class User {
+    String user;
+    int score;
+    String category;
+
+    public User (String user, String category, int Score){
+      this.user = user;
+      this.score = 0;
+      this.category = category;
+    }
+
+
+public String getUser() {
+    return user;
+}
+
+public int getScore() {
+    return score++;
+}
+public String getCategory(){
+    return category;
+}}
